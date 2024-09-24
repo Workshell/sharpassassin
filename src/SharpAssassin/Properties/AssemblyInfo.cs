178bc1af15d0115ae702558ec9e02cbfe4d1a0c2
Lloyd Kinsella <lloyd@workshell.co.uk>
2024-09-24 23:56:42 +0100

    Minor tweaks.

---
+++ src/SharpAssassin/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("SharpAssassin")]
+[assembly: AssemblyDescription("A class library for interacting with SpamAssassin and performing commands against it.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Workshell Ltd")]
+[assembly: AssemblyProduct("SharpAssassin")]
+[assembly: AssemblyCopyright("Copyright ©2024 Workshell Ltd")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyInformationalVersion("0.0.0.1")]
